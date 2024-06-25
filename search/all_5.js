a5e722690a7c17d00748ebd08cb663623a1f2cb8
vzahradnik <vzahradnik@users.noreply.github.com>
2024-06-25 14:15:00 +0000

    deploy: dd0446eb53d0be73ad2c32e5fb0853d0e08bf814

---
+++ search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['license_0',['LICENSE',['../md_LICENSE.html',1,'']]],
+  ['logging_1',['Logging',['../classLogging.html',1,'Logging'],['../classLogging.html#acc3d848a3d05076fd185cd95e9c648d5',1,'Logging::Logging()']]]
+];
---
+++ search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['logging_0',['Logging',['../classLogging.html#acc3d848a3d05076fd185cd95e9c648d5',1,'Logging']]]
+];
